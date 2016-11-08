e404c0ff5ed4b11798f613dad34f8350a6ac4510
Lawrence Enehizena <lawrence.enehizena@irokopartners.com>
2016-11-08 17:24:18 +0100

    refactor job repository
    finalize class definition

--- src/job_repository.py
+++ src/job_repository.py
@@ -3,16 +3,23 @@ import sqlite3
 class JobRepository(object):
 
     def __init__(self):
-        self.db_conn = self.connect()
+        self.connect()
 
     def insert(self, db_data):
         cursor = self.db_conn.cursor()
-        cursor.executemany(db_data);
+        cursor.executemany("INSERT into jobs(title, status, checked_at) VALUES(?, ?, ?)", db_data);
         self.disconnect()
 
     def connect(self):
-        db_conn = sqlite3.connect('database/jenkins_jobs.db')
-        return db_conn
+        db_conn = sqlite3.connect('src/database/jenkins_jobs.db')
+        self.db_conn = db_conn
+        self.createJobsTable()
 
     def disconnect(self):
         self.db_conn.close()
+
+    def createJobsTable(self):
+        cursor = self.db_conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS jobs
+                        (id INTEGER PRIMARY KEY, title text, status text, checked_at text)
+                      ''')
