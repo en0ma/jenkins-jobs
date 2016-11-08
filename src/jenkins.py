d68041c761bc2d2d673cf9066541b2fd2ed65473
Lawrence Enehizena <lawrence.enehizena@irokopartners.com>
2016-11-08 17:23:47 +0100

    define jenkins resourse

--- src/jenkins.py
+++ src/jenkins.py
@@ -1,8 +1,12 @@
-import request
+import json
+import requests
 
 class Jenkins(object):
-
-    def __init__(self, arg):
-
+    uri = "http://deadlock.netbeans.org/hudson/api/json?depth=1&tree=jobs[displayName,lastBuild[result]]"
 
     def listJobs(self):
+        response = json.loads(requests.get(self.uri).text)
+
+        if response.has_key('jobs') and len(response['jobs']) > 0:
+            return response['jobs'][0:3]
+        raise Exception('No jobs found.')
